a366a55f2b97fcd98e4c7eb2f4a25b3020bd147c
Jimmy Skinnari <jimmyskinnari@hotmail.com>
2021-04-09 00:27:53 +0200

    Update TodayForecast.js

--- weather-app/src/components/weather/TodayForecast.js
+++ weather-app/src/components/weather/TodayForecast.js
@@ -3,19 +3,17 @@ import React from 'react'
 const TodayForecast = ({todayResult, onShowLongForecast}) => {
     return (     
         <div className="TodayForecast">
-        <h1 className="city">{todayResult.name}</h1>
-        <br></br>
-        <p>Temp now:  <label className="degree">{todayResult.main.temp} °C</label></p>
-        <br></br>
-        <p>Max temp:  <label className="degree">{todayResult.main.temp_max} °C</label></p>
-        <br></br>
-        <p> Min temp:  <label className="degree">{todayResult.main.temp_min} °C</label></p>
-        <br></br>
-        <button className="btn showLongForecast" onClick={onShowLongForecast}>Show long forecast</button>
-        
+            <h1 className="city">{todayResult.name}</h1>
+            <br></br>
+            <p>Temp now:  <label className="degree">{todayResult.main.temp} °C</label></p>
+            <br></br>
+            <p>Max temp:  <label className="degree">{todayResult.main.temp_max} °C</label></p>
+            <br></br>
+            <p> Min temp:  <label className="degree">{todayResult.main.temp_min} °C</label></p>
+            <br></br>
+            <button className="btn showLongForecast" onClick={onShowLongForecast}>Show long forecast</button>       
         </div>
     ) 
 }
 
 export default TodayForecast
-
