12446bcc0f469c9d5cc9cbe6bb02e37416086c3f
Jimmy Skinnari <jimmyskinnari@hotmail.com>
2021-04-09 00:26:49 +0200

    Update App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,14 +1,10 @@
-
-
 import './App.css';
 import React, { useState, useEffect } from "react";
-
 import Header from "./components/ui/Header";
 import Footer from "./components/Footer";
 import Search from "./components/Search";
 import WeatherList from "./components/weather/WeatherList"
 import axios from 'axios';
-
 
 const App = () => {
 
@@ -17,34 +13,25 @@ const App = () => {
   const [longResults, setLongResults] = useState([]);
   const [getWeather, setGetWeather] = useState("Stockholm");
   const [showLongForecast, setShowLongForecast] = useState(true);
-  
   const [isLoading, setIsLoading] = useState(true);
 
   const search = (searchVal) =>{
-
     setGetWeather(searchVal)
   }
 
   useEffect(() => {
-
+    
     const fetchWeather = async () => {
-
       const result = await axios
         (
         `http://api.openweathermap.org/data/2.5/weather?q=${getWeather}&units=metric&appid=539ef42e6ef65b779c987dc2f30ba004`
         )
-
         const longResult = await axios
         (
         `http://api.openweathermap.org/data/2.5/forecast?q=${getWeather}&ctn=2&units=metric&appid=539ef42e6ef65b779c987dc2f30ba004`
         )
-
       setLongResults(longResult.data);
       setResults(result.data);
-
-      console.log(result.data);
-      console.log(longResult.data);
-
       setIsLoading(false);
     }
 
@@ -65,8 +52,6 @@ const App = () => {
     <Footer />
     </div>
   );
-
 }
 
 export default App;
-
